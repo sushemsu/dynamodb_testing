e4ad8b856e7d32b48d2212efe24db7bee5508b34
sushemsu <sushemsu@sushemsu>
2022-10-09 20:25:38 -0600

    update start_ddb_local.bash for better tar extraction

--- start_ddb_local.bash
+++ start_ddb_local.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-set -xe
-[[ ! -e DynamoDBLocal.jar ]] && curl -s "https://s3.us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz" | tar xzf -
+set -xepP -- -
+[[ ! -e DynamoDBLocal.jar ]] && curl -s "https://s3.us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_latest.tar.gz" | tar xz DynamoDBLocal.jar DynamoDBLocal_lib -f - 
 java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb &
